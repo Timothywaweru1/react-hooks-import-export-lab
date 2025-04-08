425333ea06e7edb897b3dfb12cade62634bc869c
Timothy Waweru <timothy.waweru1@student.moringaschool.com>
2025-04-09 02:25:48 +0300

    test4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+let username = "Liza"
+const city = "New York"
 
 function Home() {
   return (
@@ -9,3 +11,4 @@ function Home() {
     </div>
   );
 }
+export default Home
